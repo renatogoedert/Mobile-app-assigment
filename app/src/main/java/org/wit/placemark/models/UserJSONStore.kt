0cff4a76c67828bc5f6e60cae9e18e383adee874
Renato Francisco Goedert <renato.goedert@gmail.com>
2023-10-24 15:26:14 +0100

    Add User JSON store

---
+++ app/src/main/java/org/wit/placemark/models/UserJSONStore.kt
@@ -0,0 +1,72 @@
+package org.wit.placemark.models
+
+import android.content.Context
+import com.google.gson.*
+import com.google.gson.reflect.TypeToken
+import org.wit.placemark.helpers.*
+import timber.log.Timber
+import java.lang.reflect.Type
+import java.util.*
+
+const val JSON_USER_FILE = "users.json"
+val userListType: Type = object : TypeToken<ArrayList<UserModel>>() {}.type
+
+fun generateRandomUserId(): Long {
+    return Random().nextLong()
+}
+class UserJSONStore(private val context: Context) : UserStore {
+
+    var users = mutableListOf<UserModel>()
+
+    init {
+        if (exists(context, JSON_USER_FILE)) {
+            deserialize()
+        }
+    }
+
+    override fun authenticate(email: String, password: String): Boolean {
+        val user = users.find { it.email == email && it.password == password }
+        return user != null
+    }
+
+    override fun findAll(): MutableList<UserModel> {
+        return users
+    }
+
+    override fun findByEmail(email: String): UserModel? {
+        return users.find { it.email == email }
+    }
+
+    override fun create(user: UserModel) {
+        user.id = generateRandomUserId()
+        users.add(user)
+        serialize()
+    }
+
+
+    override fun update(user: UserModel) {
+        val userList = findAll() as ArrayList<UserModel>
+        val foundUser = userList.find { u -> u.id == user.id }
+        foundUser?.apply {
+            name = user.name
+            email = user.email
+            password = user.password
+        }
+        serialize()
+    }
+
+    private fun serialize() {
+        val jsonString = gsonBuilder.toJson(users, userListType)
+        write(context, JSON_USER_FILE, jsonString)
+    }
+
+    private fun deserialize() {
+        val jsonString = read(context, JSON_USER_FILE)
+        users = gsonBuilder.fromJson(jsonString, userListType)
+    }
+
+    override fun delete(user: UserModel) {
+        users.remove(user)
+        serialize()
+    }
+}
